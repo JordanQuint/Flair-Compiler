8652038a07f31285b7dcd8c5251e962268bbb414
Jordan Quint <jordanquint@hotmail.com>
2012-02-01 18:45:35 -0600

    Initial commit.

---
+++ src/compiler/project/Compiler.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package compiler.project;
+
+/**
+ *
+ * @author jordanquint
+ */
+public class Compiler {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    //validate that args[0] exists and is a filename
+    
+    //Scanner scanner = new Scanner(args[0])
+    
+    
+    //Parser
+    
+    //Semantic Analyzer
+    
+    //Optimize abstract syntax tree
+    
+    //Prepare for generation
+    
+    //Generate code
+  }
+}
